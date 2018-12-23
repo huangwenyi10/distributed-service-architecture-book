68511c2531b16be90495cca08f47e784512bf713
ay <hwy@meitu.com>
2018-12-23 14:47:45 +0800

    【项目开发】--->>> 修改文件名称

--- chapter 2/my-spring-boot/src/main/java/com/example/demo/MySpringBootApplication.java
+++ chapter 2/my-spring-boot/src/main/java/com/example/demo/MySpringBootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MySpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(MySpringBootApplication.class, args);
     }
 
 }
--- chapter 2/my-spring-boot/src/test/java/com/example/demo/MySpringBootApplicationTests.java
+++ chapter 2/my-spring-boot/src/test/java/com/example/demo/MySpringBootApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class MySpringBootApplicationTests {
 
     @Test
     public void contextLoads() {
